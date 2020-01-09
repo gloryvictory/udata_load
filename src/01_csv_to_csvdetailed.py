56b9b1620999639d31ae26824616da2e8e42fcab
zamaraev@gmail.com <zamaraev@gmail.com>
2020-01-09 11:25:10 +0500

    fields init

--- src/01_csv_to_csvdetailed.py
+++ src/01_csv_to_csvdetailed.py
@@ -23,123 +23,74 @@ from time import strftime  # Load just the strftime Module from Time
 from datetime import datetime
 import csv
 # non standard packages
-#import chardet
 
-try:
-    from sridentify import Sridentify
-except Exception as e:
-    print("Exception occurred " + str(e), exc_info=True)
-    print("try: pip install sridentify")
-
-try:
-    from chardet.universaldetector import UniversalDetector
-except Exception as e:
-    print("Exception occurred " + str(e), exc_info=True)
-    print("try: pip install chardet")
-
-
-try:
-    import shapefile
-except Exception as e:
-    print("Exception occurred " + str(e), exc_info=True)
-    print("try: pip install shapefile")
-
-
+# try:
+#     import peewee
+# except Exception as e:
+#     print("Exception occurred " + str(e), exc_info=True)
+#     print("try: pip install peewee")
 
 
 import cfg  # some global configurations
 
 
-# get first line from file
-def file_get_first_line(filename=''):
-    first_line = cfg.value_no
-    if len(str(filename)):
-        with open(filename, errors='ignore') as f:
-            first_line = f.readline().strip()
-            f.close()
-    return str(first_line)
-
-
-def get_encoding(file_dbf=''):
-    # file_dbf = 'c:\\Glory\\MyPrj\\PycharmProjects\\dbf_test\\Lov.dbf'
-    result = cfg.value_no
-    try:
-        if os.path.isfile(file_dbf):
-            detector = UniversalDetector()
-            filename = file_dbf
-            print(filename.ljust(60)),
-            detector.reset()
-            with open(filename, "rb") as f:
-                lines = f.readlines()
-                for line in lines:
-                    detector.feed(line)
-                    if detector.done: break
-                detector.close()
-                result = detector.result['encoding']
-                print(result)
-                f.close()
-    except UnicodeDecodeError:
-        result = cfg.value_error
-    return result
-
-
 def get_input_directory():
     # get from config
-    dir_shp_in_win = cfg.folder_win
-    dir_shp_in_linux = cfg.folder_linux
-    dir_shp_in = str(os.getcwd())
+    directory_in_win = cfg.folder_win_in
+    directory_in_linux = cfg.folder_linux_in
+    directory_in = str(os.getcwd())
     # if only run the script (1 argument)
     if len(sys.argv) == 1:  # there is only one argument in command line
         # Linux platform
         if _platform == "linux" or _platform == "linux2" or _platform == "darwin":
-            dir_shp_in = dir_shp_in_linux
-            return dir_shp_in
+            directory_in = directory_in_linux
+            return directory_in
         if _platform == "win32" or _platform == "win64":  # Windows or Windows 64-bit
-            dir_shp_in = dir_shp_in_win
-            return dir_shp_in
+            directory_in = directory_in_win
+            return directory_in
         else:
-            dir_shp_in = str(os.getcwd())
+            directory_in = str(os.getcwd())
             print(
-                'Input directories from config wrong: ' + dir_shp_in_win + ' or ' + dir_shp_in_linux + ' Using current directory: ' + dir_shp_in)
-        print('Input directory from a config file: ' + dir_shp_in)
-        return dir_shp_in
+                'Input directories from config wrong: ' + directory_in_win + ' or ' + directory_in_linux + ' Using current directory: ' + directory_in)
+        print('Input directory from a config file: ' + directory_in)
+        return directory_in
 
     if len(sys.argv) == 2:  # there is only one argument in command line
-        dir_shp_in = str(sys.argv[1:][0])
-        if os.path.isdir(dir_shp_in):
-            return dir_shp_in
+        directory_in = str(sys.argv[1:][0])
+        if os.path.isdir(directory_in):
+            return directory_in
         else:
             print(
-                dir_shp_in + " is not a Directory (Folder). Please specify an input directory. correctly. We use config file parameters.")
+                directory_in + " is not a Directory (Folder). Please specify an input directory. correctly. We use config file parameters.")
             if _platform == "linux" or _platform == "linux2" or _platform == "darwin":  # Linux platform
-                dir_shp_in = dir_shp_in_linux
-                return dir_shp_in
+                directory_in = directory_in_linux
+                return directory_in
             if _platform == "win32" or _platform == "win64":  # Windows or Windows 64-bit
-                dir_shp_in = dir_shp_in_win
-                return dir_shp_in
+                directory_in = directory_in_win
+                return directory_in
             else:
-                dir_shp_in = str(os.getcwd())
+                directory_in = str(os.getcwd())
                 print(
-                    'Input directories from config wrong: ' + dir_shp_in_win + ' or ' + dir_shp_in_linux + ' Using current directory: ' + dir_shp_in)
-            print('Input directory from a config file: ' + dir_shp_in)
-            return dir_shp_in
+                    'Input directories from config wrong: ' + directory_in_win + ' or ' + directory_in_linux + ' Using current directory: ' + directory_in)
+            print('Input directory from a config file: ' + directory_in)
+            return directory_in
 
     if len(sys.argv) > 2:  # there is only one argument in command line
         print("Arguments much more than 1! Please use only path as an argument. (Script.py /mnt/some_path) ")
         print(sys.argv, len(sys.argv))
         exit(1)
-    return dir_shp_in
+    return directory_in
 
 
 def get_output_directory():
     dir_out = str(os.getcwd())
     # Linux platform
     if _platform == "linux" or _platform == "linux2" or _platform == "darwin":
-        dir_out = cfg.folder_out_linux
+        dir_out = cfg.folder_linux_out
         if os.path.exists(dir_out) and os.path.isdir(dir_out):
             return dir_out
     if _platform == "win32" or _platform == "win64":  # Windows or Windows 64-bit
-        dir_out = cfg.folder_out_win
+        dir_out = cfg.folder_win_out
         if os.path.exists(dir_out) and os.path.isdir(dir_out):
             return dir_out
     else:
@@ -150,15 +101,46 @@ def get_output_directory():
     return dir_out
 
 
+def do_csv_file_in(filename_with_path=''):
+    file_name = filename_with_path.split('.')[0]
+    # print(file_name)
+    # with open(filename_with_path, newline='', encoding='utf-8') as csvfile:
+    #     reader = csv.DictReader(csvfile)
+    #     for row in reader:
+    #         print(row['FullName'], row['Length'])
+
+
+    # # Read CSV file
+    # kwargs = {'newline': ''}
+    # mode = 'r'
+    # if sys.version_info < (3, 0):
+    #     kwargs.pop('newline', None)
+    #     mode = 'rb'
+    # with open(filename_with_path, mode, **kwargs) as fp:
+    #     reader = csv.reader(fp, delimiter=cfg.csv_delimiter, quotechar='"') #delimiter=','
+    #     # next(reader, None)  # skip the headers
+    #     data_read = [row for row in reader]
+    #     print(data_read[0])
+
+    with open(filename_with_path, 'r', encoding='utf-8') as csvfile:
+     #sniff to find the format
+        filedialect = csv.Sniffer().sniff(csvfile.read(1024))
+        csvfile.seek(0)
+        #read the CSV file into a dictionary
+        dict_reader = csv.DictReader(csvfile, dialect=filedialect, quotechar='"')
+        for row in dict_reader:
+            #do your processing here
+            print(row)
+
+
 def do_csv_dir(dir_input=''):
-    _yes = cfg.value_yes
-    _no = cfg.value_no
-    _error = cfg.value_error
+    #_yes = cfg.value_yes
+    #_no = cfg.value_no
+    #_error = cfg.value_error
     # file_csv = cfg.file_csv
 
     file_csv = str(os.path.join(get_output_directory(),
                                 cfg.file_csv))  # str(strftime("%Y-%m-%d") + "_shp_info_in_folder_" + ".csv")
-    # file_csv = cfg.file_csv     #str(strftime("%Y-%m-%d") + "_shp_info_in_folder_" + ".csv")
 
     if os.path.isfile(file_csv):
         os.remove(file_csv)
@@ -186,21 +168,18 @@ def do_csv_dir(dir_input=''):
             for file in os.listdir(root):
                 file_path = str(os.path.join(root, file)).lower()
                 ext = '.'.join(file.split('.')[1:]).lower()
-                if file_path.endswith('shp')    :#ext == "shp":
+                if os.path.isfile(file_path) and file_path.endswith('csv'):#ext == "csv":
                     for key in csv_dict:
                         csv_dict[key] = ''
-                    csv_dict['DATA_SCRIPT_RUN'] = str(time.strftime("%Y-%m-%d"))
+                    #csv_dict['DATA_SCRIPT_RUN'] = str(time.strftime("%Y-%m-%d"))
                     csv_dict['FILENAME'] = file_path
-                    file_name = file_path.split('.')[0]
-
-                    csv_dict['DATA_CREATION'] = str(
-                        datetime.fromtimestamp(os.path.getctime(file_path)).strftime('%Y-%m-%d'))
-                    csv_dict['DATA_MODIFY'] = str(
-                        datetime.fromtimestamp(os.path.getmtime(file_path)).strftime('%Y-%m-%d'))
-                    csv_dict['DATA_LASTACCESS'] = str(
-                        datetime.fromtimestamp(os.path.getatime(file_path)).strftime('%Y-%m-%d'))
 
 
+                    do_csv_file_in(file_path)
+
+
+                    # csv_dict['DATA_LASTACCESS'] = str(
+                    #     datetime.fromtimestamp(os.path.getatime(file_path)).strftime('%Y-%m-%d'))
                     # Codepage DBF - work long time
                     # file_dbf = file_name + '.dbf'
                     # if os.path.isfile(file_dbf):
@@ -208,9 +187,7 @@ def do_csv_dir(dir_input=''):
                     #     csv_dict['CODEPAGE_DBF'] = get_encoding(file_dbf)
                     # else:
                     #     csv_dict['CODEPAGE_DBF'] = _no
-
                     # Get records Count from DBF - work long time
-
                     # if len(str_log):
                     csv_file_open.writerow(csv_dict)
                     # print(str(csv_dict.values()))
